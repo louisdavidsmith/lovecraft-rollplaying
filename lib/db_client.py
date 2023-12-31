2085ea95e6763fc73e98a6bea486b5fe0a1ccaf7
Louis <louisdavidsmith@gmail.com>
2023-12-31 14:21:13 -0500

    add db client

--- lib/db_client.py
+++ lib/db_client.py
@@ -3,12 +3,30 @@ from langchain.vectorstores import SQLiteVSS
 
 
 class SqlClient:
-    def __init__(self):
+    def __init__(self, game_database: str):
         self.embeddings = SentenceTransformerEmbeddings(model_name="jinaai/jina-embedding-l-en-v1")
-        self.connection = SQLiteVSS.create_connection(db_file="/data/lovecraft.db")
-        self.context_table = SQLiteVSS(table="mythos", embedding=self.embeddings, connection=self.connection)
+        self.mythos_db = SQLiteVSS.create_connection(db_file="/data/lovecraft.db")
+        self.context_table = SQLiteVSS(table="mythos", embedding=self.embeddings, connection=self.mythos_db)
         self.events_table = "adventure-events"
 
     def get_adventure_context(self, llm_output: str, top_k=3) -> str:
         context = self.context_table.similarity_search(llm_output)
         return context
+
+    def get_relevant_events(self):
+        return None
+
+    def get_relevant_character(self):
+        return None
+
+    def get_history(self):
+        return None
+
+    def update_events(self):
+        return None
+
+    def update_characters(self):
+        return None
+
+    def update_history(self):
+        return None
