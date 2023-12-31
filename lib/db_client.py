05fe74f523c7cb7dafc03e7e74dc31195ffc5994
Louis <louisdavidsmith@gmail.com>
2023-12-31 15:37:19 -0500

    add db client

--- lib/db_client.py
+++ lib/db_client.py
@@ -1,17 +1,23 @@
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.vectorstores import SQLiteVSS
+from sqlite_utils import Database
 
 
 class SqlClient:
-    def __init__(self, game_database: str):
+    def __init__(self, adventure_name: str):
         self.embeddings = SentenceTransformerEmbeddings(model_name="jinaai/jina-embedding-l-en-v1")
         self.mythos_db = SQLiteVSS.create_connection(db_file="/data/lovecraft.db")
+        self.state_db = None
+        self.history_db = Database(db_file=f"/data/{adventure_name}_history.db")
         self.context_table = SQLiteVSS(table="mythos", embedding=self.embeddings, connection=self.mythos_db)
         self.events_table = "adventure-events"
 
-    def get_adventure_context(self, llm_output: str, top_k=3) -> str:
-        context = self.context_table.similarity_search(llm_output)
-        return context
+    def intialize_adventure(self, adventure: str, save_name: str):
+        return None
+
+    def get_adventure_context(self, llm_output: str, top_k=4) -> str:
+        res = self.context_table.similarity_search(llm_output, k=top_k)
+        return " ".join([x.page_content for x in res])
 
     def get_relevant_events(self):
         return None
@@ -28,5 +34,5 @@ class SqlClient:
     def update_characters(self):
         return None
 
-    def update_history(self):
-        return None
+    def update_history(self, content: str, timestamp: float):
+        self.history_db["history"].insert({"content": content, "time_ingested_dt": timestamp})
