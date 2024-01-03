5812e34dae722b4573e58be74ebd7e1d970798b7
Louis <louisdavidsmith@gmail.com>
2024-01-03 16:27:41 -0500

    add main

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import json
 import os
+import time
 from typing import List
 
 from dotenv import load_dotenv
 from fastapi import BackgroundTasks, FastAPI
 from fastapi.responses import StreamingResponse
-from mistralai.models.chat_completion import ChatMessage
 from structlog import get_logger
 
-from lovecraft.data_models import CharacterData, Event, Scenario
-from lovecraft.db_client import SqlClient
-from lovecraft.llm_client import LLMClient
-from lovecraft.sanity_model import SanityModel
-from lovecraft.skill_model import SkillModel
-from request_types import AppendMessage, LLMResponse, NarrationRequest, ResolveSkillCheckRequest
+from module.data_models import CharacterData, Event, Scenario
+from module.db_client import SqlClient
+from module.llm_client import LLMClient
+from module.request_types import AppendMessage, LLMResponse, NarrationRequest, ResolveSkillCheckRequest
+from module.sanity_model import SanityModel
+from module.skill_model import SkillModel
 
 
 def stream_tokens(response) -> List[str]:
@@ -48,28 +48,11 @@ skill = SkillModel()
 logger.info("LoadedSkillModel")
 
 
-class ConversationHistory:
-    _messages = []
-
-    @classmethod
-    def main(self):
-        return self._messages
-
-    @classmethod
-    def append(self, message: ChatMessage):
-        self._messages.append(message)
-
-    @classmethod
-    def empty_history(self):
-        self._messages = []
-
-    @classmethod
-    def get_history(self):
-        return [{"role": message.role, "content": message.content} for message in self._messages]
-
-
 @app.get("/init_adventure")
 def init_adventure():
+    files = os.listdir("data")
+    [os.remove(f"data/{x}") for x in files if x != "lovecraft.db"]
+    db_client = SqlClient(config["adventure_name"], config["save_slot"])
     db_client.update_events(game_state.events)
     return 200
 
@@ -87,8 +70,9 @@ def get_character_data() -> CharacterData:
 # llm client
 @app.post("/narrate")
 def get_narration(request: NarrationRequest):
-    history = ConversationHistory.main()
-    ConversationHistory.append(ChatMessage(role="user", content=request.user_input))
+    ingest_time = time.time()
+    history = db_client.get_recent_history(10)
+    db_client.update_history(request.user_input, "user", ingest_time)
     context_input = " ".join([message.content for message in history])[-2000:]
     context = db_client.get_adventure_context(context_input)
     relevant_events = db_client.get_relevant_events(context_input)
@@ -99,39 +83,43 @@ def get_narration(request: NarrationRequest):
 
 @app.post("/append_message")
 def append_message(request: AppendMessage) -> int:
-    ConversationHistory.append(ChatMessage(role=request.role, content=request.content))
+    ingest_time = time.time()
+    db_client.update_history(request.content, request.role, ingest_time)
     return 200
 
 
-@app.post("/update_game_state")
-async def update_game_state(request: LLMResponse, background_task: BackgroundTasks):
+background_tasks = BackgroundTasks()
+
+
+def update_game_state(request: LLMResponse):
     past_events = db_client.get_relevant_events(request.llm_response)
     new_events = llm_client.update_event_state(past_events, request.llm_response)
     db_client.update_events([Event(**event) for event in json.loads(new_events)])
 
 
+@app.post("/update_game_state")
+async def update_state(request: LLMResponse, background_task: BackgroundTasks):
+    background_tasks.add_task(update_game_state, request)
+
+
 @app.get("/get_history")
 def get_history():
-    return ConversationHistory.get_history()
-
-
-@app.get("/empty_history")
-def empty_history():
-    ConversationHistory.empty_history()
-    return 200
+    messages = db_client.get_recent_history(10)
+    return [{"role": x.role, "content": x.content} for x in messages]
 
 
 @app.post("/narrate_resolve_skill_check")
 def resolve_skill_check(request: ResolveSkillCheckRequest):
-    history = ConversationHistory.main()
-    ConversationHistory.append(ChatMessage(role="user", content=request.user_input))
+    ingest_time = time.time()
+    history = db_client.get_recent_history(10)
+    db_client.update_history(request.content, "user", ingest_time)
     response = llm_client.resolve_skill_check(request.user_input, history, request.check_result)
     return StreamingResponse(stream_tokens(response), media_type="text/event-stream")
 
 
 @app.post("/narrate_bout_of_insanity")
 def narrate_insanity(request: LLMResponse):
-    history = ConversationHistory.main()
+    history = db_client.get_recent_history(10)
     context_input = " ".join([message.content for message in history])[-2000:]
     context = db_client.get_adventure_context(context_input)
     response = llm_client.bout_of_insanity(request.llm_response, context, character_data.sanity)
@@ -146,7 +134,7 @@ def determine_if_skill_check(request: NarrationRequest):
 @app.post("/do_skill_check")
 def do_skill_check(request: NarrationRequest):
     selected_skill = skill.select_skill(request.user_input)
-    return skill.perform_check(character_data.dict()[selected_skill])
+    return skill.perform_check(character_data.model_dump(mode="json")[selected_skill])
 
 
 @app.post("/determine_if_sanity_check")
